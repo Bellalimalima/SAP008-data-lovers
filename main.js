7bab9f8b7adddee2fbb3997c747632e230675e2f
Bellalimalima <isabellalimaelima@gmail.com>
2022-11-14 17:22:23 -0300

    Updates

---
+++ main.js
@@ -0,0 +1,73 @@
+import {
+  filtrar,
+  ordenar,
+  calculoAgregado,
+
+} from "./data.js";
+
+import data from "./data/rickandmorty/rickandmorty.js";
+
+const rickandmorty = data.results;
+
+criaCard(rickandmorty);
+
+function criaCard(personagens) {
+  const template = personagens.map((personagem) =>
+    `
+        <div class = "cardsBox">
+            <div class = "imagem">
+             <img class = "imagemCard" src="${personagem.image}" alt="Imagem personagem">
+             <div class = "nameCard">${personagem.name}</div>
+            </div>
+            <ul class = "textoCard">
+             <li>Espécie: ${personagem.species}</li>
+             <li>Gênero: ${personagem.gender}</li>
+             <li>Status: ${personagem.status}</li>
+            </ul>
+        </div>
+        `
+
+  );
+
+  document.querySelector("#exibirCards").innerHTML = template.join("");
+}
+
+document.querySelector("#especie").addEventListener("change", (event) => {
+  const valor = event.target.value;
+  const especieFiltrada = filtrar(rickandmorty, "species", valor);
+
+  const calculo = calculoAgregado(rickandmorty.length, especieFiltrada.length);
+  document.getElementById("exibirCalculo").innerHTML =
+    "Essa categoria representa " + calculo + "% dos personagens";
+
+  criaCard(especieFiltrada);
+});
+
+document.querySelector("#genero").addEventListener("change", (event) => {
+  const valor = event.target.value;
+  const generoFiltrado = filtrar(rickandmorty, "gender", valor);
+
+  const calculo = calculoAgregado(rickandmorty.length, generoFiltrado.length);
+  document.getElementById("exibirCalculo").innerHTML =
+    "Essa categoria representa " + calculo + "% dos personagens";
+
+  criaCard(generoFiltrado);
+});
+
+document.querySelector("#status").addEventListener("change", (event) => {
+  const valor = event.target.value;
+  const statusFiltrado = filtrar(rickandmorty, "status", valor);
+
+  const calculo = calculoAgregado(rickandmorty.length, statusFiltrado.length);
+  document.getElementById("exibirCalculo").innerHTML =
+    "Essa categoria representa " + calculo + "% dos personagens";
+
+  criaCard(statusFiltrado);
+});
+
+document.querySelector("#ordemalfabetica").addEventListener("change", (event) => {
+  const valor = event.target.value;
+  const ordem = ordenar(rickandmorty, valor);
+
+  criaCard(ordem);
+});
